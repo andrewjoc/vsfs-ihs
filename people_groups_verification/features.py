d21992532342edc846cfd4c0077435a5382cde62
andrewjoc <andrewo9540@gmail.com>
2022-11-06 15:25:37 -0800

    update documentation

--- people_groups_verification/features.py
+++ people_groups_verification/features.py
@@ -9,8 +9,8 @@ def prettylist(lst, remove_br=False):
     '''
     Parameter(s): List
     Parameter(s): Boolean (Default=False, returns whether the final string is to contain the breaks, coded as <br>)
-    Description: This function concatenates a list of strings in order to be viewed nicely when using Folium. It ensures that
-    the field does not run off the page when viewing points on an interactive Folium map.
+    Description: This function concatenates a list of strings in order to be viewed nicely when making an interactive Folium map. 
+    It ensures that the field does not run off the page when viewing points on an interactive Folium map.
     '''
     people_string = ''
     char_count = 0
@@ -34,8 +34,8 @@ def prettylist(lst, remove_br=False):
 
 def adm1(ppg_df, shapes, adm1_name):
     '''
-    Parameter(s): DataFrame (People groups data frame. MUST be cleaned according to other functions)
-    Parameter(s): GeoDataFrame (GeoDataFrame from adm1 shapefile. )
+    Parameter(s): DataFrame (People groups data frame. Should be the cleaned result of the find_ppg_data function.
+    Parameter(s): GeoDataFrame (GeoDataFrame from adm1 shapefile)
     Parameter(s): String (Name of the administrative level 1 subdivision name - e.g. Province)
     Note: PeopleGroups points that fall outside of an administrative region have their ADM1 subdivision encoded as "MISSING".
     These groups need to have their information manually filled in.
@@ -43,6 +43,7 @@ def adm1(ppg_df, shapes, adm1_name):
 
     subdivisions = []
     for i in np.arange(len(ppg_df.index)):
+        # Create a coordinate based on a People Group's latitude and longitude
         coordinate = Point(map(float, (ppg_df['Longitude'].iloc[i], ppg_df['Latitude'].iloc[i])))
         grouped_subdivisions = shapes[shapes['geometry'].contains(coordinate) == True].index.values
         if len(grouped_subdivisions) == 0:
@@ -50,8 +51,9 @@ def adm1(ppg_df, shapes, adm1_name):
         subdivisions.append(grouped_subdivisions[0])
     
     matches = [shapes['shapeName'].iloc[i] for i in subdivisions]
-    ppg_df[adm1_name] = matches 
-    
+    ppg_df[adm1_name] = matches
+    print(f"Please check the generated {adm1_name} column for missing values, encoded as 'MISSING'")
+    print(f"There are {sum(ppg_df[adm1_name] == 'MISSING')} unassigned people groups.")
     return ppg_df
 
 
@@ -59,11 +61,12 @@ def adm1(ppg_df, shapes, adm1_name):
 def find_ppg_data(ppg_df, country):
     '''
     Parameter(s): DataFrame (People groups data frame. MUST be cleaned according to other functions)
-    Parameter(s): String (Filepath to shp_file containing subdivision spatial data)
-    Parameter(s): String (Name of the administrative level 1 subdivision name - e.g. Province)
-    Descriptions: Finds all the
+    Parameter(s): String (Name of a Country, e.g. 'Papua New Guinea')
+    Descriptions: Finds PeopleGroups data for a specific country. 
+    Based off of 2 filters: People groups with Country column = country and People groups without a Country of Origin ('NONE or Without Homeland')
     Note: This function takes into consideration that there are PeopleGroups within a country that are not indigenous to the area. It removes
-    People Groups that have a country of origin and Deaf peoples. This function also reformats the language variable.
+    People Groups that have a country of origin and Deaf peoples. However, it is not 100% accurate so further cleaning may be needed to remove non-indigenous groups.
+    This function also reformats the language variable to remove the 3 letter language code.
     '''
     country_data = ppg_df[ppg_df['Country'] == country]
     country_data = country_data.replace({np.nan:'NONE', 'Without Homeland': 'NONE'})
@@ -81,13 +84,13 @@ def convert_to_geodataframe(df):
     """
     Parameter(s): DataFrame (clean PeopleGroups dataset)
     Output: GeoDataFrame
-    Description: This function converts the dataframe to a geodataframe by using the Latitude and Longitude columns 
+    Description: This function converts a people groups dataframe to a geodataframe by using the Latitude and Longitude columns.
     given from the PeopleGroups spreadsheet.
-    Note: This method should eventually be replaced by a pipeline function that does not already require a cleaned PeopleGroups dataset.
+    Note: This method is best used with the resulting DataFrame of the find_ppg_data() function.
     """
     if not ('Latitude' in df.columns and 'Longitude' in df.columns):
         raise ValueError('Latitude or Latitude columns not found in DataFrame.')
-    df = df[['IMB_Affinity', 'Country', 'People_Group', 'People_Name', 'Population', 'Language', 'Religion', 'Latitude', 'Longitude']]
+    df = df[['IMB Affinity Group', 'Country', 'People Group', 'People Name', 'Population', 'Language', 'Religion', 'Latitude', 'Longitude']]
     df_copy = df.copy()
     df_copy['geometry'] = gpd.points_from_xy(df.Longitude, df.Latitude)
     df_copy = gpd.GeoDataFrame(df_copy, geometry = 'geometry', crs='epsg:4326')
