448d3ef09dc36488836c8e7ca3068878272f2b30
andrewjoc <andrewo9540@gmail.com>
2023-02-01 10:33:24 -0800

    remove large peopleAreas json files

--- people_groups_verification/features.py
+++ people_groups_verification/features.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import geopandas as gpd
-from shapely.geometry import Point
-from shapely import wkt
+import folium
+from shapely import *
+from shapely.validation import make_valid
 import numpy as np
+
+
+
 
 
 def prettylist(lst, remove_br=False):
     '''
-    Parameter(s): List
-    Parameter(s): Boolean (Default=False, returns whether the final string is to contain the breaks, coded as <br>)
-    Description: This function concatenates a list of strings in order to be viewed nicely when making an interactive Folium map. 
+    Concatenates a list of strings in order to be viewed nicely when making an interactive Folium map. 
     It ensures that the field does not run off the page when viewing points on an interactive Folium map.
     '''
     people_string = ''
@@ -32,87 +34,135 @@ def prettylist(lst, remove_br=False):
     return people_string
 
 
-def adm1(ppg_df, shapes, adm1_name):
-    '''
-    Parameter(s): DataFrame (People groups data frame. Should be the cleaned result of the find_ppg_data function.
-    Parameter(s): GeoDataFrame (GeoDataFrame from adm1 shapefile)
-    Parameter(s): String (Name of the administrative level 1 subdivision name - e.g. Province)
-    Note: PeopleGroups points that fall outside of an administrative region have their ADM1 subdivision encoded as "MISSING".
-    These groups need to have their information manually filled in.
-    '''
 
-    subdivisions = []
-    for i in np.arange(len(ppg_df.index)):
-        # Create a coordinate based on a People Group's latitude and longitude
-        coordinate = Point(map(float, (ppg_df['Longitude'].iloc[i], ppg_df['Latitude'].iloc[i])))
-        grouped_subdivisions = shapes[shapes['geometry'].contains(coordinate) == True].index.values
-        if len(grouped_subdivisions) == 0:
-            grouped_subdivisions = ['MISSING']
-        subdivisions.append(grouped_subdivisions[0])
+
+def process_input_data():
+    people_areas = gpd.read_file('./data/people_areas.geojson')
+    people_areas = people_areas[['Name', 'GENC0', 'Pop', 'Ctry', 'geometry']].rename(columns={'Name': 'People Group', 'Pop': 'People Group Population', 'GENC0': 'Alpha-3 Code', 'Ctry': 'Country'})
     
-    matches = [shapes['shapeName'].iloc[i] for i in subdivisions]
-    ppg_df[adm1_name] = matches
-    print(f"Please check the generated {adm1_name} column for missing values, encoded as 'MISSING'")
-    print(f"There are {sum(ppg_df[adm1_name] == 'MISSING')} unassigned people groups.")
-    return ppg_df
-
-
-
-def find_ppg_data(ppg_df, country):
-    '''
-    Parameter(s): DataFrame (People groups data frame. MUST be cleaned according to other functions)
-    Parameter(s): String (Name of a Country, e.g. 'Papua New Guinea')
-    Descriptions: Finds PeopleGroups data for a specific country. 
-    Based off of 2 filters: People groups with Country column = country and People groups without a Country of Origin ('NONE or Without Homeland')
-    Note: This function takes into consideration that there are PeopleGroups within a country that are not indigenous to the area. It removes
-    People Groups that have a country of origin and Deaf peoples. However, it is not 100% accurate so further cleaning may be needed to remove non-indigenous groups.
-    This function also reformats the language variable to remove the 3 letter language code.
-    '''
-    country_data = ppg_df[ppg_df['Country'] == country]
-    country_data = country_data.replace({np.nan:'NONE', 'Without Homeland': 'NONE'})
-    only_indigenous = country_data[(country_data['Country of Origin'] == 'NONE')]
-    only_indigenous = only_indigenous[only_indigenous['IMB Affinity Group'] != 'Deaf Peoples']
-    clean_df = only_indigenous[['IMB Affinity Group', 'Country', 'People Group', 'People Name', 'Population', 'Language', 'Religion', 'Population', 'Latitude', 'Longitude']]
-    rename_language = clean_df.copy()
-    rename_language['Language'] = rename_language['Language'].str.extract('(^\w+(?:\s\w+)*)')[0]
-    print('This dataset may still need cleaning. Please review the returned dataset.')
-    return rename_language
+    world_populations = pd.concat(pd.read_excel('global_adm1_populations.xlsx', sheet_name=None), ignore_index=True)
+    cgaz_geometries = pd.read_csv('cgaz_geometries.csv', index_col=[0])
+    full_subnational_data = world_populations.merge(cgaz_geometries, left_on='CGAZ shape ID', right_on='shapeID')
+    full_subnational_data.geometry = full_subnational_data.geometry.apply(wkt.loads)
+    full_subnational_data = gpd.GeoDataFrame(full_subnational_data, geometry = 'geometry', crs = 'EPSG:4326')
     
+    return people_areas, full_subnational_data
 
 
-def convert_to_geodataframe(df):
-    """
-    Parameter(s): DataFrame (clean PeopleGroups dataset)
-    Output: GeoDataFrame
-    Description: This function converts a people groups dataframe to a geodataframe by using the Latitude and Longitude columns.
-    given from the PeopleGroups spreadsheet.
-    Note: This method is best used with the resulting DataFrame of the find_ppg_data() function.
-    """
-    if not ('Latitude' in df.columns and 'Longitude' in df.columns):
-        raise ValueError('Latitude or Latitude columns not found in DataFrame.')
-    df = df[['IMB Affinity Group', 'Country', 'People Group', 'People Name', 'Population', 'Language', 'Religion', 'Latitude', 'Longitude']]
-    df_copy = df.copy()
-    df_copy['geometry'] = gpd.points_from_xy(df.Longitude, df.Latitude)
-    df_copy = gpd.GeoDataFrame(df_copy, geometry = 'geometry', crs='epsg:4326')
-    return df_copy
 
 
-def find_ppg_areas(ppg_gdf, country):
+def find_all_adm1(ppg_gdf, pop_data, generate_report):
     '''
-    Parameter(s): DataFrame (People groups data frame. MUST be cleaned according to other functions)
-    Parameter(s): String (Name of a Country, e.g. 'Papua New Guinea')
-    Descriptions: Finds PeopleGroups data for a specific country. 
-    Based off of 2 filters: People groups with Country column = country and People groups without a Country of Origin ('NONE or Without Homeland')
-    Note: This function takes into consideration that there are PeopleGroups within a country that are not indigenous to the area. It removes
-    People Groups that have a country of origin and Deaf peoples. However, it is not 100% accurate so further cleaning may be needed to remove non-indigenous groups.
-    This function also reformats the language variable to remove the 3 letter language code.
+    Finds all the ADM1 boundaries that a people group intersects. 
+    ppg_gdf -> people areas dataframe
+    pop_data -> subnational data 
+    generate_report -> prints people groups that are any invalid and explains why
     '''
-    country_data = ppg_df[ppg_df['Ctry'] == country]
-    country_data = country_data.replace({np.nan:'NONE', 'Without Homeland': 'NONE'})
-    only_indigenous = country_data[(country_data['Country of Origin'] == 'NONE')]
-    only_indigenous = only_indigenous[only_indigenous['IMB Affinity Group'] != 'Deaf Peoples']
-    clean_df = only_indigenous[['IMB Affinity Group', 'Country', 'People Group', 'People Name', 'Population', 'Language', 'Religion', 'Population', 'Latitude', 'Longitude']]
-    rename_language = clean_df.copy()
-    rename_language['Language'] = rename_language['Language'].str.extract('(^\w+(?:\s\w+)*)')[0]
-    print('This dataset may still need cleaning. Please review the returned dataset.')
-    return rename_language 
+    ppl_areas = ppg_gdf.copy()
+    
+    # set both Coordinate Reference Systems (CRS) to be EPSG:4326
+    ppl_areas.crs = 'EPSG:4326'
+    pop_data.crs = 'EPSG:4326'
+    
+    
+    # Finding all overlapping boundaries
+    boundaries = []
+    for people_polygon in ppl_areas.geometry:
+        name = ppl_areas[ppl_areas.geometry == people_polygon]['People Group'].iloc[0]
+        
+        # true false series
+        overlapping_polygons = pop_data.geometry.intersects(people_polygon)        
+    
+        # from stack overflow - select series indices based on True values
+        indices = overlapping_polygons[overlapping_polygons].index.values        
+        
+        # select the names of the boundaries
+        all_boundaries_found = pop_data.query('index in @indices')['ADM1 Name'].to_list()
+        
+        if len(all_boundaries_found) == 0:
+            boundaries.append('NONE')
+        else:
+            boundaries.append(all_boundaries_found)
+            
+    # create new series that is the list of all boundaries present
+    ppl_areas['ADM1 Boundaries Present'] = boundaries     
+    
+    # add the total boundary population column
+    # ppl_areas['Total Boundary Population'] = ppl_areas['ADM1 Boundaries Present'].apply(find_total_boundary_pop)
+    ppl_areas['Total Boundary Population'] = ppl_areas['ADM1 Boundaries Present'].apply(lambda x: find_total_boundary_pop(x, pop_data))
+    
+    # add the valid column -- total boundary population >= people group population
+    ppl_areas['Valid People Group'] = ppl_areas['Total Boundary Population'] >= ppl_areas['People Group Population']
+    
+    # generate_report - add print statements to tell you which people groups are invalid and why
+    if generate_report:
+        invalid_people_groups = ppl_areas[ppl_areas['Valid People Group'] == False]
+        if invalid_people_groups.shape[0] == 0:
+            print(f'All people groups in {ppl_areas.Country.iloc[0]} are valid.')
+        
+        for ppl_group in invalid_people_groups['People Group']:
+            if invalid_people_groups[invalid_people_groups['People Group'] == ppl_group]['ADM1 Boundaries Present'].iloc[0] == 'NONE':
+                print(f'The {ppl_group} people group did not intersect with a CGAZ ADM1 boundary. They may be valid.')
+            elif invalid_people_groups[invalid_people_groups['People Group'] == ppl_group]['ADM1 Boundaries Present'].iloc[0] == 'NONE':
+                print(f'The {ppl_group} people group had a population greater than all ADM1 boundaries they intersected. They are invalid.')
+
+    return ppl_areas
+
+
+
+
+def find_total_boundary_pop(lst, subnational_data):
+    '''
+    Helper function for find_all_adm1
+    '''
+    if lst == 'NONE':
+        return np.nan
+    
+    pop_sum = 0
+    for item in lst:
+        pop_sum += subnational_data[subnational_data['ADM1 Name'] == item]['ADM1 Population'].iloc[0]
+    return pop_sum
+
+
+
+
+def validate_country(ppg_gdf, subnational_data, country, generate_report=False):
+    '''
+    Returns a Pandas dataframe validating all the people groups within a certain country
+    ppg_gdf -> full people areas dataframe
+    pop_data -> subnational data 
+    generate_report -> prints people groups that are any invalid and explains why
+    '''
+    # select subnational population data for a specific country
+    country_pop = subnational_data[subnational_data['Country'] == country].copy()
+    
+    # if geometry isn't already valid, make it valid using shapely function
+    country_pop.geometry = country_pop.apply(lambda row: make_valid(row.geometry) if not row.geometry.is_valid else row.geometry, axis=1)
+    
+    
+    # select people groups data for a specific country
+    country_ppg = ppg_gdf[ppg_gdf['Country'] == country]
+    
+    return find_all_adm1(country_ppg, country_pop, generate_report)
+
+
+
+
+
+def validate_all(ppg_gdf, subnational_data, generate_report=False):
+    '''
+    Returns a Pandas dataframe validating all people groups
+    ppg_gdf -> full people areas dataframe
+    pop_data -> subnational data 
+    generate_report -> prints people groups that are any invalid and explains why
+    '''
+    
+    return find_all_adm1(ppg_gdf, subnational_data, generate_report)
+
+
+
+
+def validate_country_widget(country):
+    people_areas, subnational_data = process_input_data()
+    country_df = validate_country(people_areas, subnational_data, country, generate_report=True)
+    return country_df
